5081ea2cf0d8473c02c7e5a8a41b7485100772f4
AnZenkova <nastyonka16@mail.ru>
2022-01-14 19:33:16 +1000

    второе задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,11 +10,26 @@ public class Main {
             Random a = new Random();
             int b = a.nextInt(100_000) + 100_000;
             dailyExpenses [i] = b;
+            System.out.println(dailyExpenses[i]);
         }
         int sumExpenses = 0;
         for (int dailyExpens : dailyExpenses) {
             sumExpenses = sumExpenses + dailyExpens;
         }
         System.out.println("Сумма трат за месяц составила " + sumExpenses + " рублей");
+        int maxExpens = -1;
+        for (int dailyExpens : dailyExpenses) {
+            if (dailyExpens > maxExpens) {
+                maxExpens = dailyExpens;
+            }
+        }
+        int minExpenses = maxExpens;
+        for (int dailyExpens : dailyExpenses) {
+            if (minExpenses > dailyExpens) {
+                minExpenses = dailyExpens;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minExpenses + " рублей. Максимальная сумма трат за день составила " + maxExpens + " рублей»");
+
     }
 }
