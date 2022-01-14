fddd3ccfe9ad0b262f98dd1746e18e29ac1676bf
AnZenkova <nastyonka16@mail.ru>
2022-01-14 19:12:15 +1000

    первое задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int [] dailyExpenses = new int [30];
+        for (int i = 0; i < 30; i++) {
+            Random a = new Random();
+            int b = a.nextInt(100_000) + 100_000;
+            dailyExpenses [i] = b;
+        }
+        int sumExpenses = 0;
+        for (int dailyExpens : dailyExpenses) {
+            sumExpenses = sumExpenses + dailyExpens;
+        }
+        System.out.println("Сумма трат за месяц составила " + sumExpenses + " рублей");
     }
 }
